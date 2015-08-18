bccfb93ebcda2a1560e111e33a9756e1ab0317d1
guoxu <grapebook0@163.com>
2015-08-18 14:26:04 +0800

    queue and question hotPotato

---
+++ hotPotato.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+
+from queue import Queue
+
+def hotPotato(namelist,num):
+	simqueue = Queue()
+	for name in namelist:
+		simqueue.enqueue(name)
+
+	while simqueue.size() > 1:
+		for i in range(num):
+			simqueue.enqueue(simqueue.dequeue())
+		
+		simqueue.dequeue()
+	
+	return simqueue.dequeue()
+
+
+if __name__ == '__main__':
+	print hotPotato(['guoxu','zhouge','xiaoshi','qing','shenshen','yuge'],4)
+	print hotPotato('A B C D E F'.split(),4)
---
+++ queue.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+#date 20150801 星期六
+
+
+#completed implementation of a dequeue ADT,use the start of list as rear,to add an element,and end of list as front ,to delete an element from queue
+class Queue:
+	def __init__(self):
+		self.items=[]
+	def is_empty(self):
+		return self.items == []
+	def enqueue(self,item):
+		self.items.insert(0,item)
+	def dequeue(self):
+		return self.items.pop()
+	def size(self):
+		return len(self.items)
+	
+if __name__ == '__main__':
+
+	q=Queue()
+	q.enqueue('hello')
+	q.enqueue('dog')
+	q.enqueue(3)
+	q.dequeue()
+	print q.items
+	
